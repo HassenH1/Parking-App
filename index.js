40b88fd958d3ff6b07dc3b10e196cbaaba149797
HassenH1 <hasansaid51@gmail.com>
2020-12-17 16:35:40 -0800

    second page

---
+++ index.js
@@ -0,0 +1,25 @@
+AOS.init();
+// let scrollPage = window.scrollY;
+// const secondPage = document.querySelector("#second-page");
+// const aboutDiv = document.querySelectorAll(".about-col");
+
+// window.onload = function () {
+//   aboutAnimation();
+// };
+
+// function aboutAnimation() {
+//   window.addEventListener("scroll", (event) => {
+//     let scroll = this.scrollY;
+//     console.log(scroll, "<========");
+//     if (scroll >= 310 && scroll <= 764) {
+//       console.log(
+//         aboutDiv[0].classList.add(
+//           "animate__animated",
+//           "animate_zoomIn",
+//           "animate__delay-5s"
+//         )
+//       );
+//       console.log(aboutDiv[0]);
+//     }
+//   });
+// }
