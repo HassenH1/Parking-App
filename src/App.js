dc0ccbd9817671edee35215e137abdcdf86d017f
HassenH1 <hasansaid51@gmail.com>
2022-02-05 22:53:52 -0800

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import     t from './'
 
 function App() {
+  const v     = 10;
+  function test =    () => {
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container border border-danger text-center">
+      Bootstrap is Working now!!
     </div>
   );
 }
